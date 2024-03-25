037c22f120a4504c9db123e5612e4389da79237a
tchisama <tchisama41@gmail.com>
2024-03-25 21:23:56 +0000

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
-/**
- * The following lines intialize dotenv,
- * so that env vars from the .env file are present in process.env
- */
+#! /home/tchisama/nodejs/bin/node
 import * as dotenv from 'dotenv';
-dotenv.config();
 
-export const sum = (a: number, b: number): number => {
-  return a + b;
-};
+import inquirer from 'inquirer';
+
+const answer = inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'number',
+    name: 'age',
+    message: 'Enter your age: ',
+  },
+  {
+    type: 'list',
+    name: 'gender',
+    message: 'What is your gender?',
+    choices: ['Male', 'Female', 'Prefer not to say'],
+  },
+]);
+
+
+dotenv.config();
